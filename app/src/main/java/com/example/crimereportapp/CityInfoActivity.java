d853f908589f723fa1ad37c65ece46b050993dbb
hongguanghe <46474783+hongguanghe@users.noreply.github.com>
2022-04-12 20:25:59 -0600

    Added Fragment to the City Info Activity

--- app/src/main/java/com/example/crimereportapp/CityInfoActivity.java
+++ app/src/main/java/com/example/crimereportapp/CityInfoActivity.java
@@ -1,16 +1,22 @@
 package com.example.crimereportapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.crimereportapp.databinding.ActivityCityInfoBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CityInfoActivity extends AppCompatActivity {
 
-//    private ActivityMainBinding binding;
+    private ActivityCityInfoBinding binding;
 
 
     @Override
@@ -21,8 +27,16 @@ public class CityInfoActivity extends AppCompatActivity {
 //        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
 //                R.id.navigation_search, R.id.navigation_explore, R.id.navigation_report)
 //                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//
+//        NavController navController = Navigation.findNavController(this, R.id.cityInfoFragmentContainer);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.navView, navController);
+//        NavigationUI.setupWithNavController(binding.bottomNavigatinView, navController);
+
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
